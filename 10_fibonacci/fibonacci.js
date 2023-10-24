a6c3511b75ab40c6b8ea6328a5a1a31b2c949af8
Sehun Park <sehunpark123@gmail.com>
2023-10-24 16:25:21 -0700

    completion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    if (x < 0) return "OOPS";
+    if (x === 0) return 0;
 
+    a = 1;
+    b = 0;
+
+    for (i = 2; i <= x; i++) {
+        let c = a + b;
+        b = a;
+        a = c;
+    }
+    return a;
 };
 
 // Do not edit below this line
